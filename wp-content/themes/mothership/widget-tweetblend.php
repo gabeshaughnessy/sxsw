91543ec5879f0ee1287890940313f3842cb8d85c
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-03-14 03:31:39 -0500

    changed tweet blend tag to boldstage

--- wp-content/themes/mothership/widget-tweetblend.php
+++ wp-content/themes/mothership/widget-tweetblend.php
@@ -1,7 +1,7 @@
 <?php 
 tweet_blender_widget(array(
     'unique_div_id' => 'tweetblender-sxsw',
-    'sources' => '#sxsw',
+    'sources' => '#boldstage',
     'refresh_rate' => 30,
     'tweets_num' => 1,
     'view_more_url' => '',
